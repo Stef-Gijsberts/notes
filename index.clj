f5c627b54373a068e807dc187601e1e2aeee65e4
Stef Gijsberts <stef@stef.link>
2025-11-08 23:39:17 +0100

    Add sine-wave example

--- index.clj
+++ index.clj
@@ -14,4 +14,5 @@
   [:ul
    [:li [:a {:href (clerk/doc-url "notes/hello")} "Hello"]]
    [:li [:a {:href (clerk/doc-url "notes/parser_combinators")} "Parser combinators"]]
-   [:li [:a {:href (clerk/doc-url "notes/clojure_subprocesses")} "Subprocesses in Clojure"]]]])
+   [:li [:a {:href (clerk/doc-url "notes/clojure_subprocesses")} "Subprocesses in Clojure"]]
+   [:li [:a {:href (clerk/doc-url "notes/linux_play_sine_wave")} "Play a sine wave in linux with a couple lines of code"]]]])
---
+++ notes/sinewave.clj
@@ -0,0 +1,12 @@
+(ns sinewave
+  (:require
+   [clojure.java.process :as p]
+   [clojure.math :refer [PI sin]]))
+
+(def aplay (p/start "aplay"))
+
+(def aplay-stdin (p/stdin aplay))
+
+(doseq [t (range 0 10000)]
+  (.write aplay-stdin
+          (byte (* 127 (sin (* 2 PI 440 (/ t 8000)))))))
