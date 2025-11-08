0a4bfde1b32f3a1d1da7d4073fd1ab29b4574cf7
Stef Gijsberts <stef@stef.link>
2025-11-08 23:42:38 +0100

    Improve sinewave.clj

--- notes/sinewave.clj
+++ notes/sinewave.clj
@@ -1,12 +1,16 @@
-(ns sinewave
-  (:require
-   [clojure.java.process :as p]
-   [clojure.math :refer [PI sin]]))
-
-(def aplay (p/start "aplay"))
-
-(def aplay-stdin (p/stdin aplay))
-
-(doseq [t (range 0 10000)]
-  (.write aplay-stdin
-          (byte (* 127 (sin (* 2 PI 440 (/ t 8000)))))))
+; Try this on your own computer!
+;
+; ```clj
+; (ns sinewave
+;   (:require
+;    [clojure.java.process :as p]
+;    [clojure.math :refer [PI sin]]))
+; 
+; (def aplay (p/start "aplay"))
+; 
+; (def aplay-stdin (p/stdin aplay))
+; 
+; (doseq [t (range 0 10000)]
+;   (.write aplay-stdin
+;           (byte (* 127 (sin (* 2 PI 440 (/ t 8000)))))))
+; ```
