655c92e1e031732e359900108c13e5b473921954
Stef Gijsberts <stef@stef.link>
2025-11-09 19:37:43 +0100

    Change sinewave example to use sample rate

--- notes/sinewave.clj
+++ notes/sinewave.clj
@@ -1,16 +1,21 @@
 ; Try this on your own computer!
 ;
+; It uses `aplay`, a simple command line tool that simply takes in audio samples
+; and plays them back.
+;
 ; ```clj
 ; (ns sinewave
 ;   (:require
 ;    [clojure.java.process :as p]
 ;    [clojure.math :refer [PI sin]]))
 ; 
-; (def aplay (p/start "aplay"))
+; (def sample-rate-hz 44000)
+; 
+; (def aplay (p/start "aplay" "-r" (str sample-rate-hz)))
 ; 
 ; (def aplay-stdin (p/stdin aplay))
 ; 
-; (doseq [t (range 0 10000)]
+; (doseq [t (range 0 sample-rate-hz)]
 ;   (.write aplay-stdin
-;           (byte (* 127 (sin (* 2 PI 440 (/ t 8000)))))))
+;           (byte (* 127 (sin (* 2 PI 440 (/ t sample-rate-hz)))))))
 ; ```
