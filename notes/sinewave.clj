62bae368ebafc9f20a163144d9da2ba385f64245
Stef Gijsberts <stef@stef.link>
2025-11-09 20:23:23 +0100

    Change sample format to 32 bit float

--- notes/sinewave.clj
+++ notes/sinewave.clj
@@ -5,26 +5,25 @@
 ;
 ; ```clj
 ; (ns sinewave
+;   (:import [java.nio ByteBuffer ByteOrder])
 ;   (:require
 ;    [clojure.java.process :as p]
 ;    [clojure.math :refer [PI sin]]))
 ; 
 ; (def sample-rate-hz 44000)
-; (def sample-format :u8)
 ; 
-; (def aplay (p/start "aplay" "-f" (name sample-format) "-r" (str sample-rate-hz)))
+; (def aplay (p/start "aplay" "-f" "FLOAT_LE" "-r" (str sample-rate-hz)))
 ; 
 ; (def aplay-stdin (p/stdin aplay))
 ; 
-; (defmulti sample->bytes (fn [sample-format _sample] sample-format))
-; 
-; (defmethod sample->bytes :u8 [_sample-format sample]
-;   (byte-array [(-> sample
-;               (/ 2.0)
-;               (+ 0.5)
-;               (* 128))]))
+; (defn float-le
+;   [sample]
+;   (let [bb (ByteBuffer/allocate 4)]
+;     (.order bb ByteOrder/LITTLE_ENDIAN)
+;     (.putFloat bb (float sample))
+;     (.array bb)))
 ; 
 ; (doseq [t (range 0 sample-rate-hz)]
 ;   (.write aplay-stdin
-;           (sample->bytes :u8 (sin (* 2 PI 440 (/ t sample-rate-hz))))))
+;           (float-le (sin (* 2 PI 440 (/ t sample-rate-hz))))))
 ; ```
